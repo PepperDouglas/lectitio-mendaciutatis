07a016f276dd65cd9e192124be8d56b35dcbe336
46707 <douglas_sebom@msn.com>
2024-09-26 18:15:00 +0200

    added controller and jwt

---
+++ LectitioMendaciutatis/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using LectitioMendaciutatis.Data;
+using LectitioMendaciutatis.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BCrypt.Net;
+
+namespace LectitioMendaciutatis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ChatContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(ChatContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("signup")]
+        public IActionResult Signup([FromBody] UserDto userDto)
+        {
+            if (userDto.Password != userDto.ConfirmPassword)
+            {
+                return BadRequest(new { message = "Passwords do not match" });
+            }
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+            var user = new User { Username = userDto.Username, PasswordHash = hashedPassword };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(new { message = "User registered successfully" });
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto loginDto)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Username == loginDto.Username);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Password, user.PasswordHash))
+            {
+                return Unauthorized(new { message = "Invalid credentials" });
+            }
+
+            var token = GenerateJwtToken(user);
+            return Ok(new { Token = token });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+    // DTOs
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+
+    public class LoginDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
