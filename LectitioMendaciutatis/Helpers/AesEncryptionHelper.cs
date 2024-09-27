59eb67212a6ad9d068ce907c224bcefdf1d09420
46707 <douglas_sebom@msn.com>
2024-09-27 18:18:50 +0200

    added aes encrypt/decrypt for messages

---
+++ LectitioMendaciutatis/Helpers/AesEncryptionHelper.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public class AesEncryptionHelper
+{
+    private readonly byte[] _key;
+    private readonly byte[] _iv;
+
+    public AesEncryptionHelper(string base64Key)
+    {
+        _key = Convert.FromBase64String(base64Key); //Decode Base64 key
+        _iv = new byte[16]; //Fixed IV (should ideally be random per message)
+    }
+
+    public string Encrypt(string plainText)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = _key;
+            aes.IV = _iv;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(plainText);
+                    }
+
+                    return Convert.ToBase64String(ms.ToArray()); //Return Base64 encoded string
+                }
+            }
+        }
+    }
+
+    public string Decrypt(string cipherText)
+    {
+        byte[] buffer = Convert.FromBase64String(cipherText);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = _key;
+            aes.IV = _iv;
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream(buffer))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cs))
+                    {
+                        return sr.ReadToEnd(); //Return decrypted text
+                    }
+                }
+            }
+        }
+    }
+}
--- LectitioMendaciutatis/Hubs/ChatHub.cs
+++ LectitioMendaciutatis/Hubs/ChatHub.cs
@@ -14,15 +14,19 @@ namespace LectitioMendaciutatis.Hubs
     public class ChatHub : Hub
     {
         private readonly ChatContext _context;
+        private readonly string _aesKey;
 
-        public ChatHub(ChatContext context)
+        public ChatHub(ChatContext context, IConfiguration configuration)
         {
             _context = context;
+            _aesKey = configuration["AESKey"];
         }
 
         // When a client connects, send the last 50 messages to them
         public override async Task OnConnectedAsync()
         {
+            var aesHelper = new AesEncryptionHelper(_aesKey);
+
             var messages = _context.ChatMessages
                 .OrderBy(m => m.Timestamp)
                 .Take(50)
@@ -30,28 +34,36 @@ namespace LectitioMendaciutatis.Hubs
 
             foreach (var message in messages)
             {
-                await Clients.Caller.SendAsync("ReceiveMessage", message.Username, message.Message);
+                string encryptedMessage = aesHelper.Encrypt(message.Message);
+                await Clients.Caller.SendAsync("ReceiveMessage", message.Username, encryptedMessage);
             }
 
             await base.OnConnectedAsync();
         }
 
         // Send a message to all connected clients
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string encryptedMessage)
         {
             if (Context.User?.Identity?.IsAuthenticated == true) {
-                // Save the message to the database
+                //Decrypt message
+                var aesHelper = new AesEncryptionHelper(_aesKey);
+                Console.WriteLine($"Received encrypted message: {encryptedMessage}");
+                string decryptedMessage = aesHelper.Decrypt(encryptedMessage);
+                Console.WriteLine($"Decrypted message: {decryptedMessage}");
+
+                //Save the message to the database
                 var chatMessage = new ChatMessage
                 {
                     Username = user,
-                    Message = message
+                    Message = decryptedMessage
                 };
 
                 _context.ChatMessages.Add(chatMessage);
                 await _context.SaveChangesAsync();
 
-                // Broadcast the message to all clients
-                await Clients.All.SendAsync("ReceiveMessage", user, message);
+                string encryptedResponseMessage = aesHelper.Encrypt(decryptedMessage);
+                //Broadcast the message to all clients
+                await Clients.All.SendAsync("ReceiveMessage", user, encryptedResponseMessage);
             } else {
                 throw new HubException("You are not authorized to send messages.");
             }
